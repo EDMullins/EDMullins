47047ad21603c19c6c6cefae0bb4afefaefb3560
Ethan Mullins <54146188+EDMullins@users.noreply.github.com>
2025-09-12 19:41:50 -0500

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("document succesfully loaded");
-    document.getElementById('btnClicky').addEventListener('click', btnClicky);
-});
-
-function btnClicky () {
-    console.log('in btnClicky()')
-    document.getElementById('btnClicky').style.transform = 'rotate(15deg)'
-}
