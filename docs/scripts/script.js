32508ef17a74a67a03282d6a7eddfd08b2d03a5b
Ethan Mullins <54146188+EDMullins@users.noreply.github.com>
2025-09-16 01:32:09 -0500

    Add files via upload
    Since I added a cookie for theme, I reworked my logic to check the theme. I gave more style to my pages.

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -5,34 +5,43 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('darkModeToggle').addEventListener('click', toggleMode);
 });
 
+// Toggle between light and dark mode/ only called after applySavedTheme()
 function toggleMode() {
     console.log('in toggleMode()')
-    const darkModeToggle = document.getElementById('darkModeToggle');
-    if (darkModeToggle.classList.contains('bi-moon-fill')) {
-        darkModeToggle.classList.replace('bi-moon-fill', 'bi-sun-fill');
+    const theme = localStorage.getItem('theme');
+
+    // if theme is light, switch to dark, and vice versa
+    if (theme === 'light') {
         setDarkTheme();
+        // uses local storage or cookies to remember theme preference
         localStorage.setItem('theme', 'dark');
-    } else {
-        darkModeToggle.classList.replace('bi-sun-fill', 'bi-moon-fill');
+    } 
+    else {
         setLightTheme();
         localStorage.setItem('theme', 'light');
     }
+    console.log('Theme set to: ' + localStorage.getItem('theme'));
+    return;
 }
 
 //Allows theme to persist across pages
 function applySavedTheme() {
     const theme = localStorage.getItem('theme');
-    const darkModeToggle = document.getElementById('darkModeToggle');
+    console.log('Theme from local storage: ' + theme);
+
     if (theme === 'dark') {
-        darkModeToggle.classList.replace('bi-moon-fill', 'bi-sun-fill');
         setDarkTheme();
-    } else {
-        darkModeToggle.classList.replace('bi-sun-fill', 'bi-moon-fill');
+    }
+    //if theme is null or light, set to light
+    else {
         setLightTheme();
     }
+    return;
 }
 
 function setDarkTheme() {
+    //icon
+    document.getElementById('darkModeToggle').classList.replace('bi-moon-fill', 'bi-sun-fill');
     //colors
     document.documentElement.style.setProperty('--primary-color', '#bb86fc');
     document.documentElement.style.setProperty('--secondary-color', '#121212');
@@ -47,10 +56,15 @@ function setDarkTheme() {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
     const img3 = document.getElementById('img3');
+    // Get the current URL
     let url = window.location.href;
+
+    // Get only the file name from the URL
     url = url.substring(url.lastIndexOf('/') + 1);
     console.log(url);
-    if (url === "index.html" || url === "#" || url === "") {
+
+    //sometimes the url is just blank instead of index.html
+    if (url === "index.html" || url === "") {
         if (img1) {
             img1.src = "imgs/landscape4.jpg";
         }
@@ -72,14 +86,18 @@ function setDarkTheme() {
             img3.src = "../imgs/landscape6.jpg";
         }
     }
+    console.log('in setDarkTheme()');
+    return;
 }
 
 function setLightTheme() {
+    //icon
+    document.getElementById('darkModeToggle').classList.replace('bi-sun-fill', 'bi-moon-fill');
     //changes colors
     document.documentElement.style.setProperty('--primary-color', '#0B6E4F');
     document.documentElement.style.setProperty('--secondary-color', '#393939');
     document.documentElement.style.setProperty('--primary-color-hover', '#094d36');
-    document.documentElement.style.setProperty('--background-color', '#fdfdfd');
+    document.documentElement.style.setProperty('--background-color', '#fffbf4');
     document.documentElement.style.setProperty('--text-color', '#222222');
     document.documentElement.style.setProperty('--card-bg', '#ffffff');
     document.documentElement.style.setProperty('--list-bg', '#ffffff');
@@ -90,9 +108,13 @@ function setLightTheme() {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
     const img3 = document.getElementById('img3');
+    // Get the current URL
     let url = window.location.href;
+    
+    // Get only the file name from the URL
     url = url.substring(url.lastIndexOf('/') + 1);
     console.log(url);
+
     if (url === "index.html" || url === "#" || url === "") {
         if (img1) {
             img1.src = "imgs/landscape1.jpg";
@@ -115,4 +137,6 @@ function setLightTheme() {
             img3.src = "../imgs/landscape3.jpg";
         }
     }
+    console.log('in setLightTheme()');
+    return;
 }
